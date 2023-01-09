e14c8e5b848b9f9c4b553fe678a8cf4613d2a52f
Patrick Yambrick <=>
2023-01-09 13:39:21 -0500

    Create Runner scss module, delete handleChange function from Editor

--- pages/components/Editor.js
+++ pages/components/Editor.js
@@ -2,11 +2,6 @@ import CodeMirror from '@uiw/react-codemirror';
 
 
 export default function Editor(props) {
-    const handleChange = (value) => {
-      if (props.title === 'HTML') {
-        props.setCodeValue(value);
-      }
-    }
     return (
         <div className="codemirror-wrapper">
           <h3>{props.title}</h3>
