f3f68b4b4fbb566b1fbe72a3b664aba5e244912f
Patrick Yambrick <=>
2023-01-09 19:39:41 -0500

    Add comments

--- pages/components/CodeResult.js
+++ pages/components/CodeResult.js
@@ -4,7 +4,10 @@ import { useEffect } from "react"
 export default function CodeRunner(props) {
     useEffect(() => {
         const doc = document.getElementById('code-runner').contentWindow.document;
+        //doc is iframe content
         doc.querySelector('html').style.backgroundColor='black';
+        //set base style
+        //TODO: match base style to chosen theme
 
     })
     return (
