c6218c92345251a6cbb75ce5fa97e0884e7ee950
Patrick Yambrick <=>
2023-01-09 19:38:36 -0500

    Style iframe content

--- pages/components/CodeResult.js
+++ pages/components/CodeResult.js
@@ -1,7 +1,14 @@
+import { useEffect } from "react"
+
+
 export default function CodeRunner(props) {
+    useEffect(() => {
+        const doc = document.getElementById('code-runner').contentWindow.document;
+        doc.querySelector('html').style.backgroundColor='black';
+
+    })
     return (
         <iframe id="code-runner">
-            {props.htmlValue}
         </iframe>
     )
 }
