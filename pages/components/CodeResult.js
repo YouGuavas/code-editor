c086a30f2f636f5974694c35b806c0ed6f8ef415
Patrick Yambrick <=>
2023-01-12 12:41:50 -0500

    Stop trying to force style updates on iframe content

--- pages/components/CodeResult.js
+++ pages/components/CodeResult.js
@@ -1,17 +1,7 @@
-import { useEffect } from "react"
 import styles from '../../styles/CodeResult.module.scss';
 
 
-export default function CodeRunner(props) {
-    useEffect(() => {
-        const doc = document.getElementById('code-runner').contentWindow.document;
-        //doc is iframe content
-        doc.querySelector('html').style.backgroundColor=props.themeBgColor;
-        doc.querySelector('body').style.margin=0;
-        //set base style
-        //TODO: match base style to chosen theme
-
-    })
+export default function CodeRunner() {
     return (
         <iframe className={styles.codeRunner} id="code-runner">
         </iframe>
